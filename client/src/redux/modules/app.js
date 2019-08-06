1b6672c2a6c9bef7f4524bd15f80d77130a4afd7
Zeyang Yi <danielzeyangyi@gmail.com>
2019-08-05 23:14:43 -0400

    add error handling

--- client/src/redux/modules/app.js
+++ client/src/redux/modules/app.js
@@ -1,5 +1,32 @@
-const reducer = (state = {}, action) => {
+const initialState = {
+  error: null
+};
+
+export const types = {
+  CLEAR_ERROR: 'APP/CLEAR_ERROR'
+};
+
+//ACTION CREATORS
+export const actions = {
+  clearError: () => ({
+    type: types.CLEAR_ERROR
+  })
+};
+
+const reducer = (state = initialState, action) => {
+  const { type, error } = action;
+  if (type === types.CLEAR_ERROR) {
+    return { ...state, error: null };
+  } else if (error) {
+    return { ...state, error: error };
+  }
+
   return state;
+};
+
+//selector:
+export const getError = state => {
+  return state.app.error;
 };
 
 export default reducer;
