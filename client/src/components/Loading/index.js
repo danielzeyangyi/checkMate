db1337af7ca456e4425a4a70472476d59209b66a
Zeyang Yi <danielzeyangyi@gmail.com>
2019-08-07 00:12:54 -0400

    add loading on scroll

---
+++ client/src/components/Loading/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export default class Loading extends Component {
+  render() {
+    return (
+      <div className='loading'>
+        <div className='loading__img' />
+        <span>Loading...</span>
+      </div>
+    );
+  }
+}
--- client/src/containers/Home/components/LikeList/index.js
+++ client/src/containers/Home/components/LikeList/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import LikeItem from '../LikeItem';
+import Loading from '../../../../components/Loading';
 import './style.css';
 
 const dataSource = [
@@ -66,19 +67,72 @@ const dataSource = [
 ];
 
 class LikeList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: dataSource,
+      loadTimes: 1
+    };
+    this.removeListener = false;
+    this.myRef = React.createRef();
+  }
+
   render() {
-    const data = dataSource;
+    const { data, loadTimes } = this.state;
     return (
-      <div className='likeList'>
+      <div ref={this.myRef} className='likeList'>
         <div className='likeList__header'>猜你喜欢</div>
         <div className='likeList__list'>
           {data.map((item, index) => {
-            return <LikeItem key={item.id} data={item} />;
+            return <LikeItem key={index} data={item} />;
           })}
         </div>
+        {loadTimes < 3 ? (
+          <Loading />
+        ) : (
+          <a href='' className='likeList__viewAll'>
+            View More
+          </a>
+        )}
       </div>
     );
   }
+
+  componentDidMount() {
+    document.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentDidUpdate() {
+    if (this.state.loadTimes >= 3) {
+      document.removeEventListener('scroll', this.handleScroll);
+      this.removeListener = true;
+    }
+  }
+
+  componentWillUnmount() {
+    if (!this.removeListener) {
+      document.removeEventListener('scroll', this.handleScroll);
+    }
+  }
+
+  // auto Load more data when user scrolls down for first 2 times
+  handleScroll = () => {
+    const scrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    const screenHeight = document.documentElement.clientHeight;
+    const likeListTop = this.myRef.current.offsetTop;
+    const likeListHeight = this.myRef.current.offsetHeight;
+    if (scrollTop >= likeListHeight + likeListTop - screenHeight) {
+      const newData = this.state.data.concat(dataSource);
+      const newLoadTimes = this.state.loadTimes + 1;
+      setTimeout(() => {
+        this.setState({
+          data: newData,
+          loadTimes: newLoadTimes
+        });
+      }, 1500);
+    }
+  };
 }
 
 export default LikeList;
